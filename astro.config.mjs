d2515de778119cde4e081188e4246cedf14c04a9
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2024-12-07 18:03:51 +0000

    hype font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import rehypeExternalLinks from 'rehype-external-links';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -13,5 +14,15 @@ export default defineConfig({
 		}),
 		mdx(),
 		sitemap()
-	]
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					content: { type: 'text', value: ' 🔗' }
+				}
+			],
+		]
+	},
 });
